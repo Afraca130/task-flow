8020b81dcfa14f948e8763d384c9253d63cdcf2b
afraca <zokatn@gmail.com>
2025-06-16 13:36:02 +0900

    feat: UsersModule에 UserLog 관련 의존성 등록 - UserLogRepository를 UserLogRepositoryPort 토큰으로 의존성 주입 설정 - UserLogController를 컨트롤러에 등록 - UserLogService 및 UserLogRepositoryPort를 exports에 추가

---
+++ apps/backend/src/users/users.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserLog } from './entities/user-log.entity';
+import { User } from './entities/user.entity';
+import { UserLogController } from './user-log.controller';
+import { UserLogRepository } from './user-log.repository';
+import { UserLogService } from './user-log.service';
+import { UserRepository } from './user.repository';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([User, UserLog])],
+    controllers: [UsersController, UserLogController],
+    providers: [UsersService, UserLogService,
+        {
+            provide: 'UserRepositoryPort',
+            useClass: UserRepository,
+        },
+        {
+            provide: 'UserLogRepositoryPort',
+            useClass: UserLogRepository,
+        },
+    ],
+    exports: [UsersService, UserLogService, 'UserRepositoryPort', 'UserLogRepositoryPort'],
+})
+export class UsersModule { }
